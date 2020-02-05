069b8b79b61b7deff45a227d43c8ddf43caa63ba
harunou <d.harunou@gmail.com>
2020-02-05 18:50:44 +0300

    Switch to production URL

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,8 @@ import {
 } from './api/websocket.client';
 import { AppStore } from './redux/store/store.typings';
 
-const WS_API_URL = 'ws://echo.websocket.org';
-// const WS_API_URL = 'wss://hometask.eg1236.com/game1/';
+// const WS_API_URL = 'ws://echo.websocket.org';
+const WS_API_URL = 'wss://hometask.eg1236.com/game1/';
 
 const socket$ = createSocket$(WS_API_URL);
 
